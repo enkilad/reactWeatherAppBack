058bfac73676f74cad1bc38c6e0a15f145578f76
Naran <enkilad@gmail.com>
2019-10-01 00:51:11 +0300

    meh

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-//Import the mongoose module
+const createError = require('http-errors');
+const express = require('express');
 const mongoose = require('mongoose');
 
-//Set up default mongoose connection
-const mongoDB = 'mongodb://localhost:27017/weatherdb';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+const indexRouter = require('./routes/index');
 
-//Get the default connection
-const db = mongoose.connection;
+const app = express();
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connect('mongodb://localhost/weather');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+app.set('view engine', 'jade');
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+module.exports = app;
