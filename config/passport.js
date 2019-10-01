679576c63338be2a3eb5b0f0ea3da1bc13e7aede
SeeKira <sk3097@mail.ru>
2019-10-01 22:13:39 +0300

    added passport

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+let User = require('../models/users');
+let md5 = require('md5');
+
+passport.use(new LocalStrategy({
+        usernameField: 'email'
+    },
+    function(username, password, done) {
+        User.findOne({ email: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect login.'
+                });
+            }
+            if (user.password !== md5(password)) {
+                return done(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return done(null, user);
+        });
+    }
+));
